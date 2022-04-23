ce60700d7dc340a811d3e3c8365225537a9db6e0
SalmonIT01 <101684618+SalmonIT01@users.noreply.github.com>
2022-04-23 13:16:04 +0700

    Create Exercise4_Panupong_P.py

---
+++ Exercise4_Panupong_P.py
@@ -0,0 +1,10 @@
+FE = 270
+GB = 155
+ICS = 90
+CP = 100
+
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming  :",CP)
