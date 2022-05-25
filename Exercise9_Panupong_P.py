4ee7720b385425121274919c10d3fb3c962a083a
SalmonIT01 <101684618+SalmonIT01@users.noreply.github.com>
2022-05-26 00:37:42 +0700

    Add files via upload

---
+++ Exercise9_Panupong_P.py
@@ -0,0 +1,8 @@
+username = input("Username:")
+password = input("Password:")
+while username !="panupong" or password !="098006":
+    print("username หรือ password ผิด,โปรดลองใหม่อีกครั้ง")
+    username = input("Username:")
+    password = input("Password:")
+
+print("เข้าสู่ระบบได้")
